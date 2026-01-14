f6e2302040070a29eeaaad4fb455a30198735ba1
LeticiAz <leticia.ig.azevedo@gmail.com>
2026-01-14 09:58:39 +0000

    Setup website structure

---
+++ src/Components/AskButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function AskButton() {
+    return (
+        <div>
+            <a
+                name="orcamento"
+                id="budget"
+                class="btn btn-primary"
+                href="#"
+                role="button"
+                >
+                    Peça o seu Orçamento
+            </a>
+        </div>
+    ) };
